eb453828be2703343a83ceb9f9b9bb3c491afc47
arivu <arivuselvan4210@gmail.com>
2024-08-27 03:32:30 -0700

    pitcher

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Home from "./page/Home/Home";
 import "./App.css";
 import Pofile from "./page/profile/Pofile";
+// import {one} from "./assets/person/"
 const App = () => {
-  
   return (
     <div className="maincon">
       <Pofile />
